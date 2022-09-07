433b650988274b39ba2cc106d0c2735a4bfac207
Alex Hernandez <alexjh221@gmail.com>
2022-09-06 19:49:54 -0500

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = (word) => {
+    let newWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+        newWord += word.charAt(i);
+    }
+    return newWord;
 };
 
 // Do not edit below this line
