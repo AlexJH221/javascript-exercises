f1880b6271321fef5b792e1c032e791517b89092
Alex Hernandez <alexjh221@gmail.com>
2022-09-06 20:46:13 -0500

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = (fahrenheit) => {
+  let celsius = 5/9 * (fahrenheit - 32);
+  return Math.round(celsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = (celsius) => {
+  let fahrenheit = 9/5 * celsius + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
