a076fdaaf66ea93fa171675150f0bbb4b9caead6
Alex Hernandez <alexjh221@gmail.com>
2022-09-06 20:10:03 -0500

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = (array, ...items) => {
+    for (let item of items) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
