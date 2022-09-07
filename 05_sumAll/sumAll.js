af3896c573f38909c73fe9211a0f2f8d860deb93
Alex Hernandez <alexjh221@gmail.com>
2022-09-06 20:26:02 -0500

    complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = (start, stop) => {
+    if (typeof start !== 'number' || typeof stop !== 'number' ||
+        start < 0 || stop < 0) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (let i = Math.min(start, stop); i <= Math.max(start, stop); i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
